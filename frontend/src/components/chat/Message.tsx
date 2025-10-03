ee2387313f44e0781a76666a33eeb235eb3eaf21
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-10-04 00:59:07 +0300

    feat: implement chat functionality with chatbot component and modal integration

--- frontend/src/components/chat/Message.tsx
+++ frontend/src/components/chat/Message.tsx
@@ -1,19 +1,24 @@
+import dayjs from './../../../node_modules/dayjs/esm/index'
+
 interface IMessage {
   from: 'user' | 'bot'
   text: string
+  timestamp: string
 }
+
 function Message({ msg, index }: { msg: IMessage; index: number }) {
   return (
-    <div className="w-full flex-1 ">
+    <div className="w-full flex-1">
       <div
         key={index}
-        className={`px-3 py-4  w-fit rounded-lg text-gray-900 max-w-xs ${
-          msg.from === 'user'
-            ? 'bg-gray-200 justify-self-end '
-            : 'bg-gray-300  '
+        className={`px-3 py-2 w-fit rounded-lg text-gray-900 max-w-xs mb-2 ${
+          msg.from === 'user' ? 'bg-gray-200 justify-self-end' : 'bg-gray-300'
         }`}
       >
-        {msg.text}
+        <div>{msg.text}</div>
+        <div className="text-xs text-gray-600 mt-2">
+          {msg.from} • {dayjs(msg.timestamp).format('HH:mm')}
+        </div>
       </div>
     </div>
   )
